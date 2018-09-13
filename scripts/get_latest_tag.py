94c9a0722990baeb41c23ed68141b1f998d2b770
cscatolini <camila.scatolini@gmail.com>
2018-09-13 15:11:12 -0300

    Push image to docker (closes #9).

---
+++ scripts/get_latest_tag.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Pitaya-Admin
+# https://github.com/topfreegames/pitaya-admin
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright © 2016 Top Free Games <backend@tfgco.com>
+
+import urllib
+import urllib2
+import json
+
+def main():
+    url = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:tfgco/pitaya-admin:pull,push"
+    response = urllib.urlopen(url)
+    token = json.loads(response.read())['token']
+
+    url = "https://registry-1.docker.io/v2/tfgco/pitaya-admin/tags/list"
+    req = urllib2.Request(url, None, {
+        "Authorization": "Bearer %s" % token,
+    })
+    response = urllib2.urlopen(req)
+    tags = json.loads(response.read())
+    last_tag = get_last_tag(tags['tags'])
+    print last_tag
+
+
+def get_last_tag(tags):
+    return max([(t.split('-')[0], t) for t in tags], key=lambda i: i[0])[1]
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/push_to_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TAG=$($TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT)
+
+docker build -t pitaya-admin .
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+docker tag pitaya-admin:latest tfgco/pitaya-admin:$TAG
+docker push tfgco/pitaya-admin:$TAG
+docker tag pitaya-admin:latest tfgco/pitaya-admin:latest
+docker push tfgco/pitaya-admin
+
+DOCKERHUB_LATEST=$(python ./scripts/get_latest_tag.py)
+
+if [ "$DOCKERHUB_LATEST" != "$TAG" ]; then
+    echo "Last version is not in docker hub!"
+    echo "docker hub: $DOCKERHUB_LATEST, expected: $TAG"
+    exit 1
+fi
